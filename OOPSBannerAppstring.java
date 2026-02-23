5a12af86807d3a806cf19ec7c7ed03d7423621d6
Shivam <hs4743@srmist.edu.in>
2026-02-23 13:25:52 +0530

    Add files via upload

---
+++ OOPSBannerAppstring.java
@@ -0,0 +1,19 @@
+package usr.bin;
+public class OOPSBannerApp {
+        public static void main(String[] args) {
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", " ****** ", " ***** "));
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "**   **", "**   **"));
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "****** ", "**     "));
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "**      ", " ***** "));
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "**          ", "     **"));
+        System.out.println(String.join(" ",
+                "**   **", "**   **", "**          ", "**   **"));
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", "**          ", " ***** "));
+    }
+}
